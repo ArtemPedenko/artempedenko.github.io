94610908a44bb8a022bcc35240e7b4d9b2386468
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-08-01 08:37:36 +0000

    Update 4 files

--- src/modules/CityList.jsx
+++ src/modules/CityList.jsx
@@ -1,34 +1,37 @@
 import { Box, InputBase, styled, Stack, Typography } from "@mui/material";
-import { List } from "react-virtualized";
+/* import { List } from "react-virtualized"; */
+//import { FixedSizeList as List } from "react-window";
 import { useSelector, useDispatch } from "react-redux";
 import CityListCard from "./cityList/CityListCard";
 
 export default function CityList() {
   const data = useSelector((state) => state.cityWishList.data);
-  const rowRenderer = ({
-    index, // Index of row
-    isScrolling, // The List is currently being scrolled
-    isVisible, // This row is visible within the List (eg it is not an overscanned row)
-    key, // Unique key within array of rendered rows
-    parent, // Reference to the parent List (instance)
-    style, // Style object to be applied to row (to position it);
-  }) => (
-    <Box key={key}>
-      {/* <Typography className="element">{data[index]}</Typography> */}
-      <CityListCard data={data[index]} />
-    </Box>
-  );
+  // const rowRenderer = ({
+  //   index, // Index of row
+  //   isScrolling, // The List is currently being scrolled
+  //   isVisible, // This row is visible within the List (eg it is not an overscanned row)
+  //   key, // Unique key within array of rendered rows
+  //   parent, // Reference to the parent List (instance)
+  //   style, // Style object to be applied to row (to position it);
+  // }) => (
+  //   <Box key={key}>
+  //     {/* <Typography className="element">{data[index]}</Typography> */}
+  //     <CityListCard data={data[index]} />
+  //   </Box>
+  // );
+
+  const Row = ({ index }) => <div>{data[index]}</div>;
 
   return (
     <Box sx={{ display: "flex", justifyContent: "center", mt: "10px" }}>
-      <List
+      {/* <List
         width={800}
         height={600}
         rowCount={data.length}
         rowHeight={25}
         rowRenderer={rowRenderer}
         className="list"
-      />
+      /> */}
     </Box>
   );
 }
