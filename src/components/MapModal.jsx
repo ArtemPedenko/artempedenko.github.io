3e8866731b607674f4fc49ab4222bc779c886257
ArtemPedenko <nubostrel@gmail.com>
2023-08-05 07:48:34 +0300

    add map

--- src/components/MapModal.jsx
+++ src/components/MapModal.jsx
@@ -1,12 +1,13 @@
 import { Modal, Box } from "@mui/material";
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 230,
-  height: 230,
+  width: 1200,
+  height: 1200,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -16,7 +17,13 @@ const style = {
 export default function MapModal(props) {
   return (
     <Modal keepMounted open={props.open} onClose={props.close}>
-      <Box sx={style}>adsadasd</Box>
+      <Box sx={style}> 
+      <YMaps>
+    <div>
+      My awesome application with maps!
+      <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+    </div>
+  </YMaps></Box>
     </Modal>
   );
 }
