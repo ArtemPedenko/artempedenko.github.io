65c7e01c3c956f422e43b5c21fa94f75d3921728
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-08-07 10:41:16 +0000

    add map to map modal

--- src/components/MapModal.jsx
+++ src/components/MapModal.jsx
@@ -1,5 +1,5 @@
 import { Modal, Box } from "@mui/material";
-import { YMaps, Map, ZoomControl } from 'react-yandex-maps';
+import { YMaps, Map, ZoomControl } from "react-yandex-maps";
 
 const style = {
   position: "absolute",
@@ -13,11 +13,21 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
-
 export default function MapModal(props) {
+  const { open, close, lat, lng } = props;
+  console.log("lat " + lat);
   return (
-    <Modal keepMounted open={props.open} onClose={props.close}>
+    <Modal keepMounted open={open} onClose={close}>
       <Box sx={style}>
+        <YMaps query={{ apikey: "e44dac90-043e-402c-8f0d-01fcbf571ff5" }}>
+          <Map
+            width="500px"
+            height="500px"
+            defaultState={{ center: [lat, lng], zoom: 11 }}
+          >
+            <ZoomControl />
+          </Map>
+        </YMaps>
       </Box>
     </Modal>
   );
