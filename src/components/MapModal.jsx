4ff6e3db4e617a0333cebbd604eedb787b3de005
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-08-07 10:45:42 +0000

    decopose map to a single component

--- src/components/MapModal.jsx
+++ src/components/MapModal.jsx
@@ -1,5 +1,5 @@
 import { Modal, Box } from "@mui/material";
-import { YMaps, Map, ZoomControl } from "react-yandex-maps";
+import YandexMap from "./YandexMap";
 
 const style = {
   position: "absolute",
@@ -15,19 +15,10 @@ const style = {
 };
 export default function MapModal(props) {
   const { open, close, lat, lng } = props;
-  console.log("lat " + lat);
   return (
     <Modal keepMounted open={open} onClose={close}>
       <Box sx={style}>
-        <YMaps query={{ apikey: "e44dac90-043e-402c-8f0d-01fcbf571ff5" }}>
-          <Map
-            width="500px"
-            height="500px"
-            defaultState={{ center: [lat, lng], zoom: 11 }}
-          >
-            <ZoomControl />
-          </Map>
-        </YMaps>
+        <YandexMap lat={lat} lng={lng} />
       </Box>
     </Modal>
   );
