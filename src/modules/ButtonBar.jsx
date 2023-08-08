c6d20397668cd5254c6beaf7a50ffcceb8f9297c
ArtemPedenko <nubostrel@gmail.com>
2023-08-08 20:49:44 +0300

    add tooltip for random button

--- src/modules/ButtonBar.jsx
+++ src/modules/ButtonBar.jsx
@@ -4,7 +4,8 @@ import {
   styled,
   Stack,
   Button,
-  Divider,
+  Tooltip,
+  Typography
 } from "@mui/material";
 import { useEffect } from "react";
 import {
@@ -53,16 +54,24 @@ export default function ButtonBar() {
         <StyledButton value="all">all</StyledButton>
         <StyledButton value="selected">selected</StyledButton>
       </ToggleButtonGroup>
-      <Button
-        variant="outlined"
-        color="secondary"
-        sx={{ backgroundColor: "primary.main", borderColor: "primary.main" }}
-        onClick={() =>
-          dispatch(setRandomCity(data[Math.floor(Math.random() * data.length)]))
-        }
-      >
-        random
-      </Button>
+      <Tooltip title={
+        <>
+          <Typography color="inherit">Tooltip with HTML</Typography>
+          <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+          {"It's very engaging. Right?"}
+        </>
+      }>
+        <Button
+          variant="outlined"
+          color="secondary"
+          sx={{ backgroundColor: "primary.main", borderColor: "primary.main" }}
+          onClick={() =>
+            dispatch(setRandomCity(data[Math.floor(Math.random() * data.length)]))
+          }
+        >
+          random
+        </Button>
+      </Tooltip>
       <RandomStreet />
     </Stack>
   );
